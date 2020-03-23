f53488b5f5f6172220079703e2280387808eea3a
elcarvalho <lessa.eric@gmail.com>
2020-03-23 17:54:21 -0300

    chore(cadastro/autenticação): autenticação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,12 +1,22 @@
 import { produce } from 'immer';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 const auth = (state = INITIAL_STATE, action) => {
   return produce(state, draft => {
     switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.token = action.payload.token;
+        draft.signed = true;
+        break;
+      }
+
       default: {
-        return state;
+        break;
       }
     }
   });
