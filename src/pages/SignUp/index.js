0a5004aeb52852f5aa8d222cea99f4ffe48b13bd
elcarvalho <lessa.eric@gmail.com>
2020-03-23 16:41:35 -0300

    chore(cadastro/autenticação): validações

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
 const SignUp = () => {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+      .email('Insira um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    password: Yup.string()
+      .min(6, 'No mínimo 6 caracteres')
+      .required('A senha é obrigatória'),
+  });
+
   const handleSubimit = data => {
     console.tron.log(data);
   };
@@ -13,7 +24,7 @@ const SignUp = () => {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubimit}>
+      <Form schema={schema} onSubmit={handleSubimit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
