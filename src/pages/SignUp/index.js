68ba516f78432cd2b48d1cb2d1990ea085c33527
elcarvalho <lessa.eric@gmail.com>
2020-03-23 19:40:16 -0300

    chore(cadastro/autenticação): cadstro na aplicação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-const SignUp = () => {
-  const schema = Yup.object().shape({
-    name: Yup.string().required('O nome é obrigatório'),
-    email: Yup.string()
-      .email('Insira um e-mail válido')
-      .required('O e-mail é obrigatório'),
-    password: Yup.string()
-      .min(6, 'No mínimo 6 caracteres')
-      .required('A senha é obrigatória'),
-  });
+import { signUpRequest } from '~/store/modules/auth/actions';
 
-  const handleSubimit = data => {
-    console.tron.log(data);
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
+const SignUp = () => {
+  const dispatch = useDispatch();
+
+  const handleSubimit = ({ name, email, password }) => {
+    dispatch(signUpRequest(name, email, password));
   };
 
   return (
