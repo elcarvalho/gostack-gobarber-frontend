a8031a282752fb8b1524ddad7f600cf77ff98c4d
elcarvalho <lessa.eric@gmail.com>
2020-03-28 10:09:34 -0300

    chore(perfil): página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  const handleSubmit = data => {};
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" type="email" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          placeholder="Sua senha atual"
+          type="password"
+        />
+        <Input name="password" placeholder="Nova senha" type="password" />
+        <Input
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+          type=""
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
